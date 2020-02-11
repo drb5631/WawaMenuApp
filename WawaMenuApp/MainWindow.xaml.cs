1573c5cc2fb46d867bd18b94e451e66f42edd207
Sierra Chiu <SierraChiu@hotmail.com>
2020-02-11 15:19:46 -0500

    MainWindow edit however

--- WawaMenuApp/MainWindow.xaml.cs
+++ WawaMenuApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WawaMenuApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This button works");
+        }
     }
 }
